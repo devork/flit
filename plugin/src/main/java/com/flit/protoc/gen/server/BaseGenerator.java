ca3893a471b6ffad0107116c33b0d0dc147abaa4
Stephen Haberman <stephen.haberman@gmail.com>
2018-09-23 19:03:39 -0500

    Remove Buffer/Indent that are used with JavaPoet.

--- plugin/src/main/java/com/flit/protoc/gen/server/BaseGenerator.java
+++ plugin/src/main/java/com/flit/protoc/gen/server/BaseGenerator.java
@@ -1,6 +1,5 @@
 package com.flit.protoc.gen.server;
 
-import com.flit.protoc.gen.Buffer;
 import com.google.protobuf.DescriptorProtos;
 import com.google.protobuf.compiler.PluginProtos;
 import com.squareup.javapoet.ClassName;
@@ -9,49 +8,21 @@ import com.squareup.javapoet.TypeSpec;
 
 import java.util.List;
 
-import static com.flit.protoc.gen.server.TypeMapper.getClassname;
-
 public abstract class BaseGenerator {
 
-  protected final Buffer b = new Buffer();
+  protected final DescriptorProtos.FileDescriptorProto proto;
   protected final DescriptorProtos.ServiceDescriptorProto service;
   protected final String javaPackage;
-  protected final String clazz;
   protected final TypeMapper mapper;
 
-  protected DescriptorProtos.FileDescriptorProto proto;
-
   protected BaseGenerator(DescriptorProtos.FileDescriptorProto proto, DescriptorProtos.ServiceDescriptorProto s, TypeMapper mapper) {
-    this.clazz = getClassname(proto);
-    this.javaPackage = proto.getOptions().getJavaPackage();
     this.proto = proto;
     this.service = s;
+    this.javaPackage = proto.getOptions().getJavaPackage();
     this.mapper = mapper;
   }
 
-  public void writeProlog() {
-    b.wn("// -------------------------------------------------------------");
-    b.wn("// Generated code from flit: Please do not modify");
-    b.wn("// -------------------------------------------------------------");
-    b.wn("\n");
-  }
-
-  public void writePackage() {
-    b.wn("package ", javaPackage, ";");
-    b.n();
-  }
-
   public abstract List<PluginProtos.CodeGeneratorResponse.File> getFiles();
-
-  public static String basename(String name) {
-    return basename(name, "\\.");
-  }
-
-  public static String basename(String name, String sep) {
-    String[] parts = name.split(sep);
-
-    return parts[parts.length - 1];
-  }
 
   protected String getFileName(String className) {
     return javaPackage.replace(".", "/") + "/" + className + ".java";
